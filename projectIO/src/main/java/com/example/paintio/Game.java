813ac0c3ff064e256e052c09b7210a359257db68
FatemehDv <fatemehdavoodvandi@gmail.com>
2023-07-28 20:51:28 +0330

    can move and paint the background

--- projectIO/src/main/java/com/example/paintio/Game.java
+++ projectIO/src/main/java/com/example/paintio/Game.java
@@ -8,8 +8,10 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.shape.Polygon;
+import javafx.scene.transform.Translate;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
     GridPane gridPane;
@@ -21,8 +23,23 @@ public class Game {
 
     public Game(GridPane gridPane){
         this.gridPane = gridPane;
+        init();
         directX = 1;
+        runMethod();
     }
+    public void runMethod(){
+        new Thread(() -> {
+            while (true){
+                move(currPos.x + directX , currPos.y + directY );
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+    }
+
     public void init (){
         gridPane.setAlignment(Pos.CENTER);
         buttons = new Button[maxSize][maxSize];
@@ -56,31 +73,101 @@ public class Game {
                 });
             }
         }
+
+        for (int i = 10; i < 13; i++){
+            for (int j = 10; j < 13; j++){
+                buttons[i][j].setStyle("-fx-background-color: GREEN");
+                list.add(new Position(i, j));
+            }
+        }
+
         currPos = new Position(12, 11);
         list.add(new Position(12, 11));
-        buttons[0][1].setStyle("-fx-background-color: GREEN");
-        buttons[0][0].setStyle("-fx-background-color: BLACK");
     }
 
     public void move(double nextX, double nextY){
         if (nextX >= maxSize || nextX < 0 || nextY >= maxSize || nextY < 0) {
             return;
         }
+        if (list.size() != 0 && checkColor(nextX, nextY, "GREEN")) {
+            completeColor(nextX, nextY);
+            return;
+        }
+
+        if (!checkColor(nextX, nextY, "GREEN")) {
+            list.add(new Position(nextX, nextY));
+        }
+
+        if (!checkColor(currPos.x, currPos.y, "GREEN"))
+            setColor(currPos.x, currPos.y, "#6dc46d");
 
         currPos.x = nextX;
         currPos.y = nextY;
+        setColor(nextX, nextY, "BLACK");
 
     }
-    public void completeColor(){
 
+    public void completeColor(double x, double y){
+        Polygon polygon = createPolygon(list, list.get(0).x, list.get(0).y);
+
+        for (int i = 0; i < polygon.getPoints().size(); i += 2) {
+            double x1 = polygon.getPoints().get(i);
+            double y1 = polygon.getPoints().get(i + 1);
+            setColor(x1, y1, "GREEN");
+            if (!exist(new Position(x1, y1))) {
+                list.add(new Position(x1, y1));
+            }
+        }
+
+        for (int i = 0; i < maxSize; i++) {
+            for (int j = 0; j < maxSize; j++) {
+                if (polygon.contains(i, j)) {
+                    if (!exist(new Position(i, j))) {
+                        list.add(new Position(i, j));
+                    }
+                    setColor(i, j, "GREEN");
+                }
+            }
+
+        }
+        currPos.x = x;
+        currPos.y = y;
+        setColor(x , y , "BLACK");
     }
-    public boolean checkColor(){
 
+    public boolean exist(Position position){
+        for (Position position1 : list) {
+            if (position.compareTo(position1) > 0)
+                return true;
+        }
+        return false;
     }
-    public void setColor(){
 
+    public boolean checkColor(double x, double y, String color){
+        return buttons[(int) x][(int) y].getStyle().equals("-fx-background-color: " + color);
     }
-    public static Polygon createPolygon(){}
 
+    public void setColor(double x, double y, String color){
+        buttons[(int) x][(int) y].setStyle("-fx-background-color: " + color);
+    }
+
+    public static Polygon createPolygon(List<Position> positions, double translateX, double translateY){
+        Polygon polygon = new Polygon();
+
+        // ایجاد مبدل ترجمه
+        Translate translate = new Translate(translateX, translateY);
+
+        // تبدیل نقاط و افزودن آنها به چندضلعی
+        for (Position point : positions) {
+            double transformedX = point.x;
+            double transformedY = point.y;
+            polygon.getPoints().addAll(transformedX, transformedY);
+        }
+
+        // اعمال ترجمه به چندضلعی
+        polygon.getTransforms().add(translate);
+
+        return polygon;
+    }
 }
 
--- projectIO/src/main/java/com/example/paintio/Main.java
+++ projectIO/src/main/java/com/example/paintio/Main.java
@@ -1,11 +1,15 @@
 package com.example.paintio;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
@@ -17,13 +21,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main extends Application {
-    GridPane gridPane;
-
-    int maxSize = 25;
-    Button[][] buttons;
-    Position currentPosition;
-
-    ArrayList<Position> list = new ArrayList<>();
+    @FXML
+    private double xOff = 0;
+    @FXML
+    private double yOff = 0;
 
     public static void main(String[] args) {
         launch(args);
@@ -31,6 +32,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
         GridPane gridPane = new GridPane();
         new Game(gridPane);
         Scene scene = new Scene(gridPane, 700, 700);
@@ -38,57 +40,8 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-    public void move(double x, double y) {
-        if (x >= maxSize || x < 0 || y >= maxSize || y < 0) {
-            System.out.println("Error");
-            return;
-        }
-        if (list.size() != 0 && buttons[(int) x][(int) y].getStyle().equals("-fx-background-color: GREEN")) {
-            Polygon polygon = createPolygon(list, list.get(0).x, list.get(0).y);
-            for (int i = 0; i < polygon.getPoints().size(); i += 2) {
-                double x1 = polygon.getPoints().get(i);
-                double y1 = polygon.getPoints().get(i + 1);
-                buttons[(int) x1][(int) y1].setStyle("-fx-background-color: GREEN");
-            }
-            for (int i = 0; i < maxSize; i++) {
-                for (int j = 0; j < maxSize; j++) {
-                    if (polygon.contains(i, j)) {
-                        buttons[i][j].setStyle("-fx-background-color: GREEN");
-                    }
-                }
-
-            }
-            currentPosition.x = x;
-            currentPosition.y = y;
-            buttons[(int) currentPosition.x][(int) currentPosition.y].setStyle("-fx-background-color: BLACK");
-            return;
-        }
-        if (!buttons[(int) x][(int) y].getStyle().equals("-fx-background-color: GREEN")) {
-            list.add(new Position(x, y));
-        }
-        buttons[(int) currentPosition.x][(int) currentPosition.y].setStyle("-fx-background-color: GREEN");
-        currentPosition.x = x;
-        currentPosition.y = y;
-        buttons[(int) currentPosition.x][(int) currentPosition.y].setStyle("-fx-background-color: BLACK");
-    }
-
-    public static Polygon createPolygon(List<Position> positions, double translateX, double translateY) {
-        Polygon polygon = new Polygon();
-
-        // ایجاد مبدل ترجمه
-        Translate translate = new Translate(translateX, translateY);
-
-        // تبدیل نقاط و افزودن آنها به چندضلعی
-        for (Position point : positions) {
-            double transformedX = point.x;
-            double transformedY = point.y;
-            polygon.getPoints().addAll(transformedX, transformedY);
-        }
-
-        // اعمال ترجمه به چندضلعی
-        polygon.getTransforms().add(translate);
-
-        return polygon;
+    @Override
+    public void stop() {
+        System.exit(0);
     }
 }
